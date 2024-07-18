66ef38482e0ab131c91d8227f18a96021e60c3e2
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-18 14:09:52 +0200

    [UPD]Added array do_list with text and done object in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            do_list: [
+                {
+                    text: 'Finire l\'esercizio del giorno',
+                    done: true,
+                }, {
+                    text: 'Studiare ciò che ho visto a lezione',
+                    done: true,
+                }, {
+                    text: 'Fare la spesa',
+                    done: false,
+                }, {
+                    text: 'Bere un buon caffè',
+                    done: true,
+                }, {
+                    text: 'Rankare fino a diamante su OW2',
+                    done: false,
+                }, {
+                    text: 'Vedere la nuova stagione di "The Boys"',
+                    done: false,
+                }, {
+                    text: 'Uscire con gli amici a prendere una birra',
+                    done: true,
+                }, {
+                    text: 'Preparare una bella cena alla mia ragazza',
+                    done: true,
+                }
+            ]
+        }
+    }
+}).mount('#app')
+    
